2d04b9e409bc68ad5c893f9a4344d21058279220
Anton Stremovskyy <stremovskyy@me.com>
2023-09-23 23:25:53 +0300

    Update SwiftyDeflate.swift

--- Sources/SwiftyDeflate/SwiftyDeflate.swift
+++ Sources/SwiftyDeflate/SwiftyDeflate.swift
@@ -10,14 +10,14 @@ import zlib
 
 extension Data {
         // Decompression
-    func deflateDecompress() throws -> Data {
+    public func deflateDecompress() throws -> Data {
         let inflater = InflateStream(windowBits: -15)
         let decompressedBytes = try inflater.write(Array(self), flush: true)
         return Data(decompressedBytes)
     }
     
         // Compression
-    func deflateCompress(level: Int = Int(Z_DEFAULT_COMPRESSION)) throws -> Data {
+    public func deflateCompress(level: Int = Int(Z_DEFAULT_COMPRESSION)) throws -> Data {
         let deflater = DeflateStream(level: level, windowBits: -15)
         let compressedBytes = try deflater.write(Array(self), flush: true)
         return Data(compressedBytes)
